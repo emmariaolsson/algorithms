88dec787c7dfe1a5a936b1203d0de98b5ff8a08f
Emma Olsson <emmaolss@kth.se>
2020-02-14 15:58:46 +0100

    Measure branch coverage for maximum_flow_bfs manually #5

--- algorithms/graph/maximum_flow_bfs.py
+++ algorithms/graph/maximum_flow_bfs.py
@@ -26,11 +26,14 @@ import queue
 import math
 
 def maximum_flow_bfs(adjacency_matrix):
+    branches = [0]*11
+
     #initial setting
     new_array = copy.deepcopy(adjacency_matrix)
     total = 0
 
     while(1):
+        branches[0] = 1
         #setting min to max_value
         min = math.inf
         #save visited nodes
@@ -47,38 +50,65 @@ def maximum_flow_bfs(adjacency_matrix):
 
         #BFS to find path
         while(bfs.qsize() > 0):
+            branches[1] = 1
             #pop from queue
             src = bfs.get()
             for k in range(len(new_array)):
+                branches[2] = 1
                 #checking capacity and visit
                 if(new_array[src][k] > 0 and visited[k] == 0 ):
+                    branches[3] = 1
                     #if not, put into queue and chage to visit and save path
                     visited[k] = 1
                     bfs.put(k)
                     path[k] = src
+                else:
+                    branches[4] = 1
             
         #if there is no path from src to sink
         if(visited[len(new_array) - 1] == 0):
+            branches[5] = 1
             break
+        else:
+            branches[6] = 1
         
         #initial setting
         tmp = len(new_array) - 1
 
         #Get minimum flow
         while(tmp != 0):
+            branches[7] = 1
             #find minimum flow
             if(min > new_array[path[tmp]][tmp]):
+                branches[8] = 1
                 min = new_array[path[tmp]][tmp]
+            else:
+                branches[9] = 1
             tmp = path[tmp]
+            
 
         #initial setting
         tmp = len(new_array) - 1
 
         #reduce capacity
         while(tmp != 0):
+            branches[10] = 1
             new_array[path[tmp]][tmp] = new_array[path[tmp]][tmp] - min
             tmp = path[tmp]
 
         total = total + min
-    
+    codeCoverage(branches)
     return total
+
+def codeCoverage(branches):
+    print("Check out this coverage:")
+    total = 0
+    for x in range(11):
+        if branches[x] == 1:
+            total+=1
+            print("Branch #{} has been checked".format(x))
+        else:
+            print("Branch #{} has not been checked".format(x))
+    percent = (total/11)*100
+    print("Coverage percent: {}%".format(percent))
+
