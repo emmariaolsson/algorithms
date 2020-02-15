9858518375d904c6aa0fa235af8b7a852b763c2b
Emma Olsson <emmaolss@kth.se>
2020-02-15 15:40:00 +0100

    test: Add tworking test case for  non-existing linked list #10

--- tests/test_palindrome_dict.py
+++ tests/test_palindrome_dict.py
@@ -35,10 +35,8 @@ class TestSuite(unittest.TestCase):
         self.l1.next.next.next = Node('B')
         self.assertFalse(is_palindrome_dict(self.l1))
     #Suppose to cover branch 0 (if not head)
-    def test_if_palindrome_dict_empty(self):
-        self.l2 = Node(None) #This case needs to be scrutinized. Node() returns error.
-        run = is_palindrome_dict(self.l2)
-        self.assertTrue(run)
+    def test_if_palindrome_dict_None(self):
+        self.assertTrue(is_palindrome_dict(None))
 
     #Suppose to cover branch 1 (if not head.next)    
     def test_if_palindrome_dict_single(self):
