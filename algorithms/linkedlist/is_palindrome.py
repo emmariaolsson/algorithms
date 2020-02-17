03494564c8aa88e2dd2a5aec7549fedfb946b29e
Emma Olsson <emmaolss@kth.se>
2020-02-17 11:20:42 +0100

    refactor: Add helpfunction fill_dict which is called in while-loop #13

--- algorithms/linkedlist/is_palindrome.py
+++ algorithms/linkedlist/is_palindrome.py
@@ -1,54 +1,3 @@
-def is_palindrome(head):
-    if not head:
-        return True
-    # split the list to two parts
-    fast, slow = head.next, head
-    while fast and fast.next:
-        fast = fast.next.next
-        slow = slow.next
-    second = slow.next
-    slow.next = None  # Don't forget here! But forget still works!
-    # reverse the second part
-    node = None
-    while second:
-        nxt = second.next
-        second.next = node
-        node = second
-        second = nxt
-    # compare two parts
-    # second part has the same or one less node
-    while node:
-        if node.val != head.val:
-            return False
-        node = node.next
-        head = head.next
-    return True
-
-
-def is_palindrome_stack(head):
-    if not head or not head.next:
-        return True
-
-    # 1. Get the midpoint (slow)
-    slow = fast = cur = head
-    while fast and fast.next:
-        fast, slow = fast.next.next, slow.next
-
-    # 2. Push the second half into the stack
-    stack = [slow.val]
-    while slow.next:
-        slow = slow.next
-        stack.append(slow.val)
-
-    # 3. Comparison
-    while stack:
-        if stack.pop() != cur.val:
-            return False
-        cur = cur.next
-
-    return True
-
-
 def is_palindrome_dict(head):
     """
     This function builds up a dictionary where the keys are the values of the list,
@@ -62,15 +11,14 @@ def is_palindrome_dict(head):
     d = {1: [0,1,5,6], 2: [2,4], 3: [3]}
     '3' is the middle outlier, 2+4=6, 0+6=6 and 5+1=6 so we have a palindrome.
     """
-    if not head or not head.next:
+    if not head: 
+        return True 
+    if not head.next:
         return True
     d = {}
     pos = 0
     while head:
-        if head.val in d.keys():
-            d[head.val].append(pos)
-        else:
-            d[head.val] = [pos]
+        fill_dict(head, d, pos)
         head = head.next
         pos += 1
     checksum = pos - 1
@@ -87,3 +35,9 @@ def is_palindrome_dict(head):
         if middle > 1:
             return False
     return True
+
+def fill_dict(head, d, pos):
+    if head.val in d.keys():
+        d[head.val].append(pos)
+    else:
+        d[head.val] = [pos]
