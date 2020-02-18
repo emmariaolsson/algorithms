86b9d5e9f38bd43dd8a3fb2da7db661a69682f8e
Emma Olsson <emmaolss@kth.se>
2020-02-18 11:56:55 +0100

    test: Delete unneccessary test from sudoku solution hashtable #10

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -29,23 +29,6 @@ class TestSudokuValidator(unittest.TestCase):
             )
         )
 
-        #Suppose to cover branch 4
-        self.assertFalse(
-            sudoku_validator.valid_solution_hashtable(
-                [
-                    [5, 5, 5, 5, 5, 5, 5, 5, 5],
-                    [6, 7, 2, 1, 9, 5, 3, 4, 8],
-                    [1, 9, 8, 3, 4, 2, 5, 6, 7],
-                    [8, 5, 9, 7, 6, 1, 4, 2, 3],
-                    [4, 2, 6, 8, 5, 3, 7, 9, 1],
-                    [7, 1, 3, 9, 2, 4, 8, 5, 6],
-                    [9, 6, 1, 5, 3, 7, 2, 8, 4],
-                    [2, 8, 7, 4, 1, 9, 6, 3, 5],
-                    [3, 4, 5, 2, 8, 6, 1, 7, 9]
-                ]
-            )
-        )
-
         #Original test case
         self.assertTrue(
             sudoku_validator.valid_solution_hashtable(
