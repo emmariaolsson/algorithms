7e54cf4a09b54fc3442f82c9013e37198b5de2bf
Emma Olsson <emmaolss@kth.se>
2020-02-19 13:04:58 +0100

    Add a last test case for grid_add != 45 for 100% branch coverage #10

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -58,6 +58,22 @@ class TestSudokuValidator(unittest.TestCase):
                     [5, 8, 7, 4, 1, 9, 6, 3, 5],
                     [5, 4, 5, 2, 8, 6, 1, 7, 9]
                 ]))
+
+        #Supposed to cover branch 12 (if grid_add != 45)
+        self.assertFalse(
+            sudoku_validator.valid_solution_hashtable(
+                [
+                    [5, 3, 4, 6, 7, 8, 10, 1, 2],
+                    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+                    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+                    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+                    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+                    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+                    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+                ]))
+
         #Original test case
         self.assertTrue(
             sudoku_validator.valid_solution_hashtable(
